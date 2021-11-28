e736720463aa9a3b76be2e25ea074678a8aed3c3
Alice <alicev871203@gmail.com>
2021-11-28 23:35:28 +0800

    heroku

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import uvicorn
 from fastapi import FastAPI
-from router import artical
 from fastapi.middleware.cors import CORSMiddleware
+from router import artical
+from db import models
+from db.database import engine
 # from fastapi.responses import FileResponse
 
 app = FastAPI(
@@ -12,23 +14,20 @@ app = FastAPI(
 )
 app.include_router(artical.router)
 
-
-@app.get("/")
-def root():
-    return {"title": "Hello World ,Hi Alice"}
-
-
 if __name__ == "__main__":
     uvicorn.run("app:app", port=5000, reload=True)
 
 origins = [
-    'http://localhost:3000'
+    'http://localhost:3000',
+    "*"
 ]
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_method=["*"],
-#     allow_headers=['*']
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=['*']
+)
+
+models.Base.metadata.create_all(engine)
--- router/artical.py
+++ router/artical.py
@@ -3,6 +3,8 @@ from sqlalchemy.orm.session import Session
 from router.schemas import ArticalRequestSchema, ArticalResponseSchema
 from db.database import get_db
 from db import db_articals
+from typing import List
+
 
 router = APIRouter(
     prefix='/api/v1/articals',
@@ -15,17 +17,17 @@ def create(request: ArticalRequestSchema, db: Session = Depends(get_db)):
     return db_articals.create(db, request)
 
 
-@router.get('/all')
+@router.get('/all', response_model=List[ArticalResponseSchema])
 def get_all_articals(db: Session = Depends(get_db)):
     return db_articals.get_all(db)
 
 
-@router.get("/")
+@router.get("/", response_model=ArticalResponseSchema)
 def get_all_articals(db: Session = Depends(get_db)):
     return db_articals.get_all(db)
 
 
-@router.get("/id/{id}")
+@router.get("/id/{id}", response_model=List[ArticalResponseSchema])
 def get_artical_by_id(id: int, db: Session = Depends(get_db)):
     return db_articals.get_product_by_id(id, db)
 
